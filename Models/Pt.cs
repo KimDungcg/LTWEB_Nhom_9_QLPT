ae032e784ec5528742793428d55cab8491525107
ErciNguyen <88387748+ErciNguyen@users.noreply.github.com>
2023-04-12 00:27:08 +0700

    btktr

---
+++ Models/Pt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace btktr.Models;
+
+public partial class Pt
+{
+    public string MaPt { get; set; } = null!;
+
+    public string? TenPt { get; set; }
+
+    public string? Avatar { get; set; }
+
+    public string? NamKn { get; set; }
+
+    public int? TuoiPt { get; set; }
+
+    public string? MaNoiHoc { get; set; }
+
+    public string? MaTrinhDo { get; set; }
+
+    public decimal? GiaPt { get; set; }
+
+    public string? MaCalam { get; set; }
+
+    public virtual ICollection<HoaDon> HoaDons { get; } = new List<HoaDon>();
+
+    public virtual CaLam? MaCalamNavigation { get; set; }
+
+    public virtual NoiHoc? MaNoiHocNavigation { get; set; }
+
+    public virtual TrinhDo? MaTrinhDoNavigation { get; set; }
+
+    public virtual ICollection<CauLacBo> MaClbs { get; } = new List<CauLacBo>();
+
+    public virtual ICollection<Khoahoc> MaKhoaHocs { get; } = new List<Khoahoc>();
+}
