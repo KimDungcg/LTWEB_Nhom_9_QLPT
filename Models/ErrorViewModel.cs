219d588c9674bf930ade1843d4815de031e07add
ErciNguyen <88387748+ErciNguyen@users.noreply.github.com>
2023-04-12 00:07:54 +0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace btktr.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
