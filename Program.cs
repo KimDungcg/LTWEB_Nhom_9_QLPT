5eec543cef0fc3a7660790c925eb10e43d9832f6
KimDungcg <92260780+KimDungcg@users.noreply.github.com>
2023-04-17 17:31:24 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using BTL_QLPT_CT.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+var connect = builder.Configuration.GetConnectionString("BtlQlptContex");
+builder.Services.AddDbContext<BtlQlptContext>(x => x.UseSqlServer
+(connect));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
